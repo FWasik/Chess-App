213746373d856933d7d1b4543966c7b0208225ba
FWasik <80278631+FWasik@users.noreply.github.com>
2022-02-18 16:54:12 +0100

    heroku commit#2

--- main.py
+++ main.py
@@ -488,5 +488,5 @@ api.add_resource(
 )
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
+    port = os.environ.get('PORT', 5000)
     app.run(debug=False, host='0.0.0.0', port=port)
